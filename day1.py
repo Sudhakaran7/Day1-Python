7139213560febc9a533418e6fbc90276583b60ac
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2022-02-11 17:58:12 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,22 @@
+#single line comment
+'''
+multi
+line
+comments
+'''
+'''number1=10
+number2=20
+answer=number1+number2
+print(number1)'''
+ 
+#Datatypes and Type Conversions
+'''
+name='shreyasi' #string Datatype
+Age=20 #integer Datatype
+height=173.5 #Float Datatype
+print(type(Age))'''
+ 
+number1=3
+number2=5.6
+output=number1+number2
+print(output)
